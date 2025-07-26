df95f562b57c21266f2e3c7d53094c1db30d01ac
MellowMancer <wyatharth@gmail.com>
2025-07-26 21:10:53 +0530

    generated keys (which are not on the repo obviously) + signed the app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,12 +7,24 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystorePropertiesFile = rootProject.file("key.properties")
+
+val keystoreProperties = Properties().apply {
+    if (keystorePropertiesFile.exists()) {
+        keystorePropertiesFile.inputStream().use {
+            load(it)
+        }
+    }
+}
+
 android {
     namespace = "com.example.fynans"
     // --- FIX #1: Override the default Flutter SDK version ---
     // We are hardcoding this to 34 to solve the "lStar not found" error.
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
+
+    println("--> The value of flutter.minSdkVersion is: ${flutter.minSdkVersion}")
 
     sourceSets {
         getByName("main").java.srcDirs("src/main/kotlin")
@@ -28,15 +42,30 @@ android {
     defaultConfig {
         applicationId = "com.example.fynans"
         minSdk = flutter.minSdkVersion
-        // --- Also update the target SDK to match the compile SDK ---
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+            storeFile = if (keystoreProperties.getProperty("storeFile") != null) {
+                file(keystoreProperties.getProperty("storeFile"))
+            } else {
+                null
+            }
+            storePassword = keystoreProperties.getProperty("storePassword")
+        }
+    }
+
     buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
