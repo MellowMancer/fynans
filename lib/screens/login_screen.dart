0564d7328284be6b98400892d3495dd0f42aa5f0
Mello <wyatharth@gmail.com>
2026-05-24 18:14:22 +0530

    chore: remove aws imports from LoginScreen and AppDrawer
    Remove amplify_flutter and amplify_auth_cognito imports that are no
    longer available in pubspec.yaml after Amplify removal in tasks 1-2.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:amplify_flutter/amplify_flutter.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({
