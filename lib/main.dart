ff62c7793b90bd3ba8201678399e4f80f53a1aa4
Mello <wyatharth@gmail.com>
2026-05-24 18:08:56 +0530

    refactor: remove amplify initialization and auth routing from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,71 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:fynans/models/transaction.dart';
 import 'package:fynans/main_screen.dart';
-import 'package:fynans/screens/login_screen.dart';
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-import 'package:amplify_flutter/amplify_flutter.dart';
-
-import 'amplify_outputs.dart';
 
 Future<void> main() async {
-  try {
-    WidgetsFlutterBinding.ensureInitialized();
-    await Hive.initFlutter();
-    Hive.registerAdapter(TransactionAdapter());
-    await Hive.openBox<Transaction>('transactions');
-    await _configureAmplify();
-    runApp(const MyApp());
-  } on AmplifyException catch (e) {
-    runApp(Text("Error configuring Amplify: ${e.message}"));
-  }
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(TransactionAdapter());
+  await Hive.openBox<Transaction>('transactions');
+  runApp(const MyApp());
 }
 
-Future<void> _configureAmplify() async {
-  try {
-    await Amplify.addPlugin(AmplifyAuthCognito());
-    await Amplify.configure(amplifyConfig);
-    safePrint('Successfully configured');
-  } on Exception catch (e) {
-    safePrint('Error configuring Amplify: $e');
-  }
-}
-
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool _isAuthenticated = false;
-  bool _isCheckingAuth = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkAuthStatus();
-  }
-
-  Future<void> _checkAuthStatus() async {
-    try {
-      final authSession = await Amplify.Auth.fetchAuthSession();
-      if (mounted) {
-        setState(() {
-          _isAuthenticated = authSession.isSignedIn;
-          _isCheckingAuth = false;
-        });
-      }
-    } catch (e) {
-      if (mounted) {
-        setState(() {
-          _isAuthenticated = false;
-          _isCheckingAuth = false;
-        });
-      }
-    }
-  }
-
 
   @override
   Widget build(BuildContext context) {
@@ -108,59 +54,7 @@ class _MyAppState extends State<MyApp> {
           ),
         ),
       ),
-      routes: {
-        '/': (context) => _AuthWrapper(
-          isCheckingAuth: _isCheckingAuth,
-          isAuthenticated: _isAuthenticated,
-          onAuthCheck: _checkAuthStatus,
-        ),
-        '/home': (context) => const MainScreen(),
-        '/login': (context) => const LoginScreen(),
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == '/home') {
-          return MaterialPageRoute(
-            builder: (context) => const MainScreen(),
-          );
-        }
-        return null;
-      },
+      home: const MainScreen(),
     );
-  }
-}
-
-class _AuthWrapper extends StatefulWidget {
-  final bool isCheckingAuth;
-  final bool isAuthenticated;
-  final Future<void> Function() onAuthCheck;
-
-  const _AuthWrapper({
-    required this.isCheckingAuth,
-    required this.isAuthenticated,
-    required this.onAuthCheck,
-  });
-
-  @override
-  State<_AuthWrapper> createState() => _AuthWrapperState();
-}
-
-class _AuthWrapperState extends State<_AuthWrapper> {
-  @override
-  void initState() {
-    super.initState();
-    // Re-check auth status when this widget is mounted
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      widget.onAuthCheck();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (widget.isCheckingAuth) {
-      return const Scaffold(
-        body: Center(child: CircularProgressIndicator()),
-      );
-    }
-    return widget.isAuthenticated ? const MainScreen() : const LoginScreen();
   }
 }
